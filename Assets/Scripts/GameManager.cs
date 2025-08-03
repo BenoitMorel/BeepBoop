df8ec3f9c9a491d7c389ae9eb7f8baa6de3eab4e
BenoitMorel <benomorel@gmail.com>
2025-08-03 17:01:12 +0200

    Restart when R is pressed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        // if R is pressed, restart the game
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // restart the game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
